366f8c535e1110576c4b14782b1c1bfdd402a508
Nikhil Yadav <nikhilyadav.prof@gmail.com>
2026-01-25 18:26:49 +0530

    Implemented create resume route

--- cilent/src/dashboard/index.jsx
+++ cilent/src/dashboard/index.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import AddResume from "./components/AddResume";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="p-10 md:px-20 lg:px-32">
+      <h2 className="font-bold text-3xl">My Resume</h2>
+      <p>Start Creating AI resume to your next Job role</p>
+      <div
+        className="grid grid-cols-2 
+        md:grid-cols-3 lg:grid-cols-5 gap-5
+        mt-10"
+      >
+        <AddResume />
+      </div>
+    </div>
+  );
 }
